60a808beb62236d9d9da07216d220d28b07eae6b
Ben <ben@solero.me>
2019-04-11 00:54:07 +0100

    Plugin abstract class interface

---
+++ Houdini/Plugins/__init__.py
@@ -0,0 +1,29 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class IPlugin(ABC):
+    """
+    Plugin interface which all plugins *must* implement.
+    """
+
+    @property
+    @abstractmethod
+    def author(self):
+        """The plugin's author which is usually a nickname and an e-mail address."""
+
+    @property
+    @abstractmethod
+    def version(self):
+        """The version of the plugin."""
+
+    @property
+    @abstractmethod
+    def description(self):
+        """Short summary of the plugin's intended purpose."""
+
+    @abstractmethod
+    async def ready(self):
+        """
+        Called when the plugin is ready to function.
+        """
