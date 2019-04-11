ce4603d49689d641914d863281630e1227b23336
Ben <ben@solero.me>
2019-04-11 01:32:10 +0100

    Remove test handler from login module

--- Houdini/Handlers/Login/Login.py
+++ Houdini/Handlers/Login/Login.py
@@ -1,15 +1,9 @@
 from Houdini import Handlers
-from Houdini.Handlers import XTPacket, XMLPacket
-from Houdini.Converters import CredentialsConverter, CommaConverter
+from Houdini.Handlers import XMLPacket
+from Houdini.Converters import CredentialsConverter
 
 
 @Handlers.handler(XMLPacket('login'))
 async def handle_login(p, credentials: CredentialsConverter):
     username, password = credentials
     p.logger.info('{}:{} is logging in!'.format(username, password))
-
-
-@Handlers.handler(XTPacket('t', 'c'), pre_login=True)
-@Handlers.player_in_room(100)
-async def handle_test(p, numbers: CommaConverter):
-    print(list(numbers))
