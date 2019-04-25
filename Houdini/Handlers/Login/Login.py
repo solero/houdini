cf1c41ecd8dedc6f6dccedf8b24f6a0e7e69158c
Ben <ben@solero.me>
2019-04-25 17:30:59 +0100

    Placeholder packages for game handlers

--- Houdini/Handlers/Login/Login.py
+++ Houdini/Handlers/Login/Login.py
@@ -1,9 +1,14 @@
 from Houdini import Handlers
 from Houdini.Handlers import XMLPacket
-from Houdini.Converters import CredentialsConverter
+from Houdini.Converters import CredentialsConverter, VersionChkConverter
 
 
 @Handlers.handler(XMLPacket('login'))
 async def handle_login(p, credentials: CredentialsConverter):
     username, password = credentials
     p.logger.info('{}:{} is logging in!'.format(username, password))
+
+
+@Handlers.handler(XMLPacket('verChk'))
+async def handle_version_check(p, version: VersionChkConverter):
+    p.logger.info('Version: {}'.format(version))
