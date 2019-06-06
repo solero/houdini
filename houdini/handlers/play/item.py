63fd52e93d1c279de0ee940f3841dbb2a71f7f58
Ben <ben@solero.me>
2019-06-07 00:45:52 +0100

    Get inventory handler

---
+++ houdini/handlers/play/item.py
@@ -0,0 +1,8 @@
+from houdini import handlers
+from houdini.handlers import XTPacket
+
+
+@handlers.handler(XTPacket('i', 'gi'))
+@handlers.allow_once()
+async def handle_get_inventory(p):
+    await p.send_xt('gi', *p.data.inventory.keys())
