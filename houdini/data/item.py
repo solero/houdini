651896c20ed7dc0b6a9595803394e718f82a0b43
ro <44623254+rsakeys@users.noreply.github.com>
2019-06-09 02:56:22 +0200

    Item type check methods created within Item class

--- houdini/data/item.py
+++ houdini/data/item.py
@@ -15,6 +15,34 @@ class Item(db.Model):
     tour = db.Column(db.Boolean, nullable=False, server_default=db.text("false"))
     release_date = db.Column(db.Date, nullable=False, server_default=db.text("now()"))
 
+    def is_color(self):
+        return self.type == 1
+
+    def is_head(self):
+        return self.type == 2
+
+    def is_face(self):
+        return self.type == 3
+
+    def is_neck(self):
+        return self.type == 4
+
+    def is_body(self):
+        return self.type == 5
+
+    def is_hand(self):
+        return self.type == 6
+
+    def is_feet(self):
+        return self.type == 7
+
+    def is_flag(self):
+        return self.type == 8
+
+    def is_photo(self):
+        return self.type == 9
+
+
 
 class PenguinItem(db.Model):
     __tablename__ = 'penguin_item'
