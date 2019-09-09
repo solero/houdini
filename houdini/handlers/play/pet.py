81f40e70660210ab0f472ba54223014e41b897c1
Ben <ben@solero.me>
2019-09-09 22:29:42 +0100

    Placeholder for puffle name check handler

--- houdini/handlers/play/pet.py
+++ houdini/handlers/play/pet.py
@@ -5,3 +5,9 @@ from houdini.handlers import XTPacket
 @handlers.handler(XTPacket('p', 'getdigcooldown'), pre_login=True)
 async def handle_get_dig_cooldown(p):
     await p.send_xt('getdigcooldown', 1)
+
+
+@handlers.handler(XTPacket('p', 'checkpufflename'))
+async def handle_check_puffle_name_with_response(p, puffle_name):
+    name_ok = puffle_name.isalnum()
+    await p.send_xt('checkpufflename', puffle_name, int(name_ok))
