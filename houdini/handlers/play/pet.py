99237c928090ce2b7710f2fe38fcca89cf5323eb
Ben <ben@solero.me>
2019-10-09 01:16:25 +0100

    Add placeholder puffle handlers

--- houdini/handlers/play/pet.py
+++ houdini/handlers/play/pet.py
@@ -1,5 +1,6 @@
 from houdini import handlers
 from houdini.handlers import XTPacket
+from houdini.constants import ClientType
 
 
 @handlers.handler(XTPacket('p', 'getdigcooldown'), pre_login=True)
@@ -11,3 +12,13 @@ async def handle_get_dig_cooldown(p):
 async def handle_check_puffle_name_with_response(p, puffle_name):
     name_ok = puffle_name.isalnum()
     await p.send_xt('checkpufflename', puffle_name, int(name_ok))
+
+
+@handlers.handler(XTPacket('p', 'pg'), client=ClientType.Vanilla)
+async def handle_get_player_puffles(p, penguin_id: int, room_type: str):
+    await p.send_xt('pg')
+
+
+@handlers.handler(XTPacket('p', 'pg'), client=ClientType.Legacy)
+async def handle_get_player_puffles_legacy(p, penguin_id: int):
+    await p.send_xt('pg')
