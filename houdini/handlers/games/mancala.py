802f34e326bb443c51dbc9cf9aff8ea48e692652
loading <49134864+load1n9@users.noreply.github.com>
2019-12-11 16:04:35 -0500

    added the start of the mancala handler

---
+++ houdini/handlers/games/mancala.py
@@ -0,0 +1,11 @@
+#from houdini import handlers
+#from houdini.handlers import XTPacket
+#from houdini.handlers.games.table import table_handler
+#from houdini.data.room import MancalaLogic
+
+#@handlers.handler(XTPacket('gz', ext='z'))
+#@table_handler(MancalaLogic)
+#async def handle_get_game(p):
+ #   await p.send_xt('gz', p.table.get_string())
+
+
