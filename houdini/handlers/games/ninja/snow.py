a2999ad4854c9a6b28e9d7f39f12aba58108e5fb
AllinolCP <56921534+AllinolCP@users.noreply.github.com>
2022-03-10 11:11:35 +0700

    Update snow.py

--- houdini/handlers/games/ninja/snow.py
+++ houdini/handlers/games/ninja/snow.py
@@ -6,12 +6,12 @@ from houdini.handlers.play.stampbook import get_player_stamps_string
 
 @handlers.handler(XTPacket('i#ngi'))
 @handlers.cooldown(5)
-async def ninja_get_inventory(self, p):
+async def ninja_get_inventory(p):
     p.inventory = await PenguinItemCollection.get_collection(p.id)
     await p.send_xt('gi', *p.inventory.keys())
 
 @handlers.handler(XTPacket('st#ngps'))
 @handlers.cooldown(5)
-async def ninja_get_penguin_stamps(self, p):
+async def ninja_get_penguin_stamps(p):
     p.stamps = await PenguinStampCollection.get_collection(p.id)
     await p.send_xt('gps', p.id, await get_player_stamps_string(p, p.id))
